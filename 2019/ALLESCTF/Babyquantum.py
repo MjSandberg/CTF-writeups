82c6d21f64557edea3c2899cef7ba5ae722efb72
MjSandberg <40609955+MjSandberg@users.noreply.github.com>
2019-08-25 14:16:08 +0200

    Solved

---
+++ 2019/ALLESCTF/Babyquantum.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+
+whole = ""
+browser = webdriver.Chrome('C:\webdriver\chromedriver.exe')
+browser.get("http://hax.allesctf.net:5000/")
+
+
+elem = browser.find_element_by_name("circuit")
+elem.send_keys("- H U1 H D\nX H U2 H D")            # Deutsch Algo
+elem2 = browser.find_element_by_class_name("ui button")
+elem2.click()
+
+for i in range(0,210):
+    browser.delete_all_cookies()
+    browser.add_cookie({'domain': 'hax.allesctf.net',
+      'httpOnly': False,
+      'name': 'randomness',
+      'path': '/',
+      'secure': False,
+      'value': str(i)})
+    browser.refresh()
+
+
+    string = browser.find_element_by_css_selector("body").text
+    whole += string[0]
+print(whole)
+browser.quit()
+
+
+whole[::-1]
